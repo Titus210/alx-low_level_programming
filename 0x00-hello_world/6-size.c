bfccf7c67d850afbdd5deb202b8f0efbc701dc26
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-02 16:19:37 -0700

    Hello World Project

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - printing size of data types
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	char c;
+	long l;
+	long long ll;
+	float f;
+	printf("Size of int: %lu byte(s)\n", (unasigned long)sizeof(a));
+	printf("Size of an char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an lond: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return(0);
+}
