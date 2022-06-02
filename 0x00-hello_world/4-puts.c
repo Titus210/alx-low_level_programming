8d0ff3d49be6bcfc3502eeb81b5cc89ac2a05926
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-02 15:14:37 -0700

    Hello World Project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
-
-
-
-/**
- *
- *  * main - prints the phras Programming is like building a multilingual puzzle
- *
- *  Return0
- *
- *    */
-
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
