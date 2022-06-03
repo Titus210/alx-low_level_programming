be65919820ec67232f27789cf7eaaffe4b52a96d
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-02 17:30:42 -0700

    Hello World Project

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-1
-* Return: 1 
+ * main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-1
+ * Return: 1  
 */
 int main(void)
 {
