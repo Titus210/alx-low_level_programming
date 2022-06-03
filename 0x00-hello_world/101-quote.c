455ef1120db7a869cbb2e4a3033d6cb2dc9b10a3
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-02 17:26:25 -0700

    Hello World Project

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
 * main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-1
 * Return: 1 
-* */
+*/
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
