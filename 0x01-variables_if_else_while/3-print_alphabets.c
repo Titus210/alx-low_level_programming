4915c44b252260bba556847a8f29d13d795060f8
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-04 13:46:35 -0700

    Variables and control structures

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+ * main - program that prints the alphabet in lowercase.
  * Return: Always 0 (Success)
 */
 int main(void)
 {
 	int c;
-
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
 	for (c = 'A'; c <= 'Z'; c++)  
