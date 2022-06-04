28c8d4bc08e61adc6a8182642dc76183af95998d
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-04 12:54:10 -0700

    Variable and control structures

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("Last digit of %d is %d ", n, n % 10);
 	if (n % 10 > 5)
 	{
-		printf("greater than 5\n");
+		printf("and is greater than 5\n");
 	}
 	else if (n % 10 == 0)
 	{
