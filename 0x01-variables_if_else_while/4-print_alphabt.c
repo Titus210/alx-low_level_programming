78bfaac63716853f255baf788bf4fbfd8313fb25
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-04 14:21:34 -0700

    Variables and control structures

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/** 
+ * main -  program that prints the alphabet in lowercase
+ * Return: 0 when success
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
