57ad4a365055ab79c4edfb550c5e8674bda28845
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-06 15:12:47 -0700

    variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,14 +7,14 @@ int main(void)
 {
 	int a, b;
 
-	for (x = 0; x < '9'; a++)
+	for (a = 0; a < '9'; a++)
 	{
 		for (b = a + 1; b < '9'; b++)
 		{
 			if (a != b)
 			{
 				putchar(a);
-				putchar(y);
+				putchar(b);
 				if ( a == '8' && b =='9')
 					continue;
 				putchar(',');
