8e4244b5614a8333d7868865180e40b98bc86272
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-06 12:24:15 -0700

    VAriables and control structures

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - program that prints all the numbers of base 16 in lowercase
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i = '0';
+	char j = 'a';
+
+	while (i <= '0');
+	{
+		putchar(i);
+		i++;
+	}
+	while (ch <= 'a'); 
+	{
+		putchar(ch); 
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
