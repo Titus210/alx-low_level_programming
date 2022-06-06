9b56b9f89f2e53f2e87a64ee7ebdbf377ea95384
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-06 14:14:41 -0700

    variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints all possible different combinations of three digits.
+ * Return: 0;
+ */
+int main(void)
+{
+	int a,b,c;
+
+	for (a = '0'; a < '9; a++)
+	{
+		for ( b = b + 1; b <='9'; b++)
+		{
+			for ( c = b + 1; c <='9'; c++)
+			{
+				if ((b != a) != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(z);
+					if ( a == '7' && b == '8')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+			
