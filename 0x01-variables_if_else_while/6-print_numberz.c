b122b0fcef405fffdb0fa3dd2107132456d012b7
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-06 11:10:08 -0700

    Variables and control structures

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ * return: 0 (on success)
+ */
+int main(void)
+{
+	int i= '0';
+
+	while(i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
