1a2552e93e8eb89adcc4e299f1bf95eea46058b0
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-06 15:02:25 -0700

    variables

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of two two-digit numbers.
+ * Return:0 (when success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b <100; b++)
+		{
+			if( a < b)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b / 10) + 48);
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
