ff1a5fc14fcfce3666ba1bb46d7fbc334734e530
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-07 13:52:34 -0700

    Nested functions and loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * is_lower -checks for lowercase character.
+ * Return: 1 if lowercase else 0
+ */
+int _islower(int c)
+{
+	char ch;
+	int lower = 0;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'c')
+		{
+			lower = 1;
+		}
+	}
+	return (lower);
+}
