8d0c7b5549b2539afbf2ddf70fbb20cef98ac5cc
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-07 15:32:47 -0700

    Nested loops and functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ *_abs - computes the absolute value of an integer.
+ *@r:declared int
+ *Return: absolute value of r
+ */
+int _abs(int r)
+{
+	if (r >= 1)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}
