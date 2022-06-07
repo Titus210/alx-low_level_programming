c2ebe9a79d9ab1b29f8c2c56d3126a783fcc7088
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-07 09:18:39 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ * Returns: none
+ */
+(void) print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	putchar('\n');
+}
