a767f83fd10fd031cbace1464bfbd8865a930cdf
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-07 14:51:27 -0700

    Nested loops and functions

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/*print_sign - prints the sign of a number
+ *@n: an input
+ *Retur: 0,1,-1 depending on condition
+ */
+int print_sign(int n)
+{
+	int number;
+
+	if (n > 1)
+	{
+		number = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		number = 0;
+		_putchar('0');
+	}
+	else 
+	{
+		number = -1;
+		_putchar('-');
+	}
+	return (number);
+}
