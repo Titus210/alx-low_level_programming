57d1845a4a57f3052ef57257487e919a44280ba6
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-07 12:02:38 -0700

    Nested functions and loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_alphabet -  program that prints _putchar
  * Return: nothing
@@ -9,8 +10,7 @@ void print_alphabet(void)
 	
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		_putchar(ch;)
+		_putchar(ch);
 	}
-
 	_putchar('\n');
 }
