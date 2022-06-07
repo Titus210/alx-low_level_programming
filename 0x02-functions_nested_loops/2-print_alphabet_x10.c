e0f16db8218f10dfb9ede6ea642741e9dcdfe9a8
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-07 10:42:12 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - prints 10 times the alphabet, in lowercase,
+ *Return: 0(when success)
+ */
+ int main(void)
+{
+	char ch;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
