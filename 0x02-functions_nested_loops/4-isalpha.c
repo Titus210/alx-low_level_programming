d484833a4a715072003264b3a0d888455653c495
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-07 14:32:10 -0700

    Nested loops and functions

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+ *  _isalpha - checks for alphabetic character.
+ *  @c: An input
+ *  Return: 1 if lower or upper otherwhise 0
+ */
+int _isalpha(int c)
+{
+	char lowercase, uppercase;
+	int letter = 0;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase+=)
+	{
+		for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+		{
+			if (c == lowercase || c == uppercase)
+			{
+				letter = 1;
+			}
+		}
+	}
+	return (letter);
+}
