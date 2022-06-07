824546e862108da9bf4579f350d96e993e2535e7
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-07 08:55:08 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * print_alphabet -  program that prints _putchar
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch)
+	}
+
+	putchar('\n');
+}
