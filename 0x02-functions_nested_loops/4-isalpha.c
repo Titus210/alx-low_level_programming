19a0887006ad350c74a2362bb09fe75862db4a76
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-08 10:14:53 -0700

    Loops and control structures

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*_isalpha- check if lowercase or uppercase
- *Description: function returns 1 if the character is a
- @c: An input
- *Return: 1 if lower or upper otherwhise 0
+ * _isalpha- check if lowercase or uppercase
+ * Description: function returns 1 if the character is a
+ * @c: An input
+ * Return: 1 if lower or upper otherwhise 0
  */
 int _isalpha(int c)
 {
