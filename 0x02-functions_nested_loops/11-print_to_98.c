738350c1695dd5aaf0677b7714a96ded628a3289
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-07 17:17:11 -0700

    Nested loops and functions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_to_98 - all natural numbers from n to 98
+ *@n: integer
+ *Return: null
+ */
+void print_to_98(int n);
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			putchar("%d", n);
+			if (n == 98)
+				continue;
+			putchar(", ");
+		}
+		putchar("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			putchar("%d", n);
+			if (n == 98)
+				continue;
+			putchar(", ");
+		}
+		_putchar("\n");
+	}
+}
