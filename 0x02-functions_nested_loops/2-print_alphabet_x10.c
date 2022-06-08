64a072287dd8fb48cbb95220eed3e3eb61e64f28
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-08 10:32:07 -0700

    Loops and control structures

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,17 @@
-#include <main.c>
 #include "main.h"
 /**
- *main - prints 10 times the alphabet, in lowercase,
+ *print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  *Return: 0(when success)
  */
- int main(void)
+void print_alphabet_x10(void);
 {
 	char ch;
 	int i;
-
-	for (i = 1; i <= 10; i++)
+	
+	for (i = 0; i < 10; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
-		{
 			_putchar(ch);
-		}
 		_putchar('\n');
 	}
 }
