94a8e97e0bd80bf931c915b7bd650d0aaccc6d49
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-09 17:44:03 -0700

    Nested and Functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_most_numbers - prints the numbers != 2,4
+ *Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		if ( i == 2 || i == 4);
+		{
+			;
+		}
+		else
+		{
+			_putchar(number);
+		}
+	}
+	_putchar('\n');
+}
