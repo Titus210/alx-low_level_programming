f5f4a8b2b11feca40b92aada0527143f5b9e9f65
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-09 17:04:02 -0700

    Nested loops and functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Prints no btwn 0-9
+ * Returns:0
+ */
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
