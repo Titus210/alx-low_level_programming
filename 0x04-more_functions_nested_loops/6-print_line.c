f09ccfcc82dbe6050f05515fb5c93eb4fc34a80b
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-09 18:43:37 -0700

    Functions and nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#inlude "main.h"
+/**
+ *print_line - raws a straight line in the terminal.
+ *@n:number of lines
+ *Return: void
+ */
+void print_line(int n)
+{
+	int lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (lines = 1; lines <= n; lines++)
+		{
+			_putchar('_')
+		}
+		_putchar('\n');
+	}
+}
