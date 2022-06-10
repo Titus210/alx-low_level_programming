4d19eb907dc25c12b7760a42b51b9b542614978a
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-09 17:15:53 -0700

    Nested and Functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * print_numbers - Prints no btwn 0-9
- * Returns:Void
+ * Returns: void
  */
 void print_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 		_putchar(i);
 	_putchar('\n');
