1e6e0e133b784fa737b9458b798bb2b78706d1a3
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-06-13 18:46:42 -0700

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - print char of string
+ * @str: string input
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	len -= 1;
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
