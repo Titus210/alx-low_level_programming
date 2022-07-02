336b35d038f9528ca80bedd12315658fc19ee39b
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-07-01 17:54:31 -0700

    Allocating memory for array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc- initializes memory spaces with zero.
+ * @nmemb: string 1.
+ *  @size: string 2, concatenated to 1
+ *  Return: If nmemb or size is 0,  else NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *newArray;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	newArray = malloc(nmemb * size);
+	if (newArray == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*(newArray + i) = 0;
+	}
+	return (newArray);
+}
