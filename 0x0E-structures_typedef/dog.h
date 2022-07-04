a3967e36e45499aec7567f2f77086b1f7bb86700
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-07-04 13:24:46 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dogs!
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: does anyone ever really own a dog?
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif
