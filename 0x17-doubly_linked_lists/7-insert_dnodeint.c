cdd688b457b432b8a58b29f98b31848cc78dd517
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-09-02 18:47:24 -0700

    add

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,32 +8,28 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *new;
-	dlistint_t *temp = *h;
+	dlistint_t *new, *cursor;
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new || !h)
+	if (h == NULL)
 		return (NULL);
-	new->n = n;
-	new->next = NULL;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	for (i = 0; temp && i < idx; i++)
+	cursor = *h;
+	while (idx > 1 && cursor && cursor->next)
 	{
-		if (i == idx - 1)
-		{
-			if (temp->next == NULL)
-				return (add_dnodeint_end(h, n));
-			new->next = temp->next;
-			new->prev = temp;
-			temp->next->prev = new;
-			temp->next = new;
-			return (new);
-		}
-		else
-			temp = temp->next;
+		cursor = cursor->next;
+		idx--;
 	}
-	return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	if (idx > 1 || cursor == NULL)
+		return (NULL);
+	new->n = n;
+	if (cursor->next != NULL)
+		cursor->next->prev = new;
+	new->prev = cursor;
+	new->next = cursor->next;
+	cursor->next = new;
+	return (new);
 }
-
